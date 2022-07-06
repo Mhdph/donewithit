ebcbfe7102feaec3fedc29dcc46edbdde23d0286
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-07-06 14:36:21 +0430

    Complate Welcome Screen

---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
+
+function WelcomeScreen() {
+  return (
+    <ImageBackground
+      blurRadius={10}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" color="primary"></AppButton>
+        <AppButton title="Register" color="secondary"></AppButton>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+
+  logo: {
+    height: 100,
+    width: 100,
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+});
+
+export default WelcomeScreen;
