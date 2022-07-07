e78df58c7c9c48421cec0f8a5bd94aac5cd876ba
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-07-07 16:09:02 +0430

    Detail And View Screen Complate

---
+++ app/screens/ListingDetailsScreen.tsx
@@ -0,0 +1,50 @@
+import { Image, StyleSheet, View } from "react-native";
+import React from "react";
+import AppText from "../components/AppText";
+import colors from "../config/colors";
+import ListItem from "../components/ListItem";
+
+export default function ListingDetailsScreen() {
+  return (
+    <View>
+      <Image
+        style={styles.image}
+        source={require("../assets/jacket.jpg")}
+      ></Image>
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red Jacket For Sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Mahdi Parivash"
+            subTitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 30,
+  },
+});
