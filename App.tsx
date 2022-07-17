99ee1c5c0a05333fadaeb95619aff4750fe16a15
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-07-17 20:33:51 +0430

    fix loading issue

--- App.tsx
+++ App.tsx
@@ -5,7 +5,10 @@ import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
+import AppLoading from "expo-app-loading";
 import { AuthContext } from "./app/auth/context";
+import { getUser } from "./app/auth/storage";
+
 interface User {
   name: string;
   email: string;
@@ -22,7 +25,24 @@ interface UserState {
 
 export default function App() {
   const [user, setUser] = useState<TUser>(null);
-  const authContext = useContext(AuthContext);
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await getUser();
+
+    if (!user) return;
+
+    setUser(user);
+  };
+
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreUser}
+        onFinish={() => setIsReady(true)}
+        onError={() => console.log("Error")}
+      />
+    );
 
   return (
     <AuthContext.Provider value={{ user, setUser } as UserState}>
